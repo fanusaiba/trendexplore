609086a570bc85b448139cd4d4d67fbfb546fd14
Fanusaiba <nusaiba.fathima1703@gmail.com>
2026-02-25 01:22:25 +0530

    Fix Dashboard useEffect error

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
+import { api } from "../api";
 
 export default function Dashboard() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/trends?country=US&category=All", {
-      credentials: "include", // IMPORTANT
-    })
-      .then((res) => res.json())
-      .then((data) => setData(data));
+    api
+      .get("/api/trends?country=US&category=All")
+      .then((res) => setData(res.data));
   }, []);
 
   if (!data) return <p>Loading...</p>;
