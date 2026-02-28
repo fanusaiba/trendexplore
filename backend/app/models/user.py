77b54f44842376836cdb1f34760f2cda30612888
Fanusaiba <nusaiba.fathima1703@gmail.com>
2026-03-01 02:05:31 +0530

    Fix outdated BeanieBaseUser import

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,18 @@
 from beanie import Document
-from fastapi_users.db import BeanieBaseUser
+from fastapi_users import schemas
+from fastapi_users.db import BeanieUserDatabase
+from pydantic import EmailStr
+from typing import Optional
+from bson import ObjectId
+from pydantic import BaseModel
 
-class User(BeanieBaseUser,Document):
-    pass
+
+class User(Document):
+    email: EmailStr
+    hashed_password: str
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Settings:
+        name = "users"
