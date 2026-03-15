8a89b4d8e2793847e1b32f210c4cc365289b8e4f
Fanusaiba <nusaiba.fathima1703@gmail.com>
2026-03-16 00:18:47 +0530

    fix user model settings

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,12 @@
-from fastapi_users_db_beanie import BeanieBaseUser
 from beanie import Document
+from fastapi_users.db import BeanieBaseUser
+from pydantic import Field
+from typing import Optional
+from uuid import UUID, uuid4
 
 
-class User(BeanieBaseUser,Document):
+class User(BeanieBaseUser, Document):
+    id: UUID = Field(default_factory=uuid4)
+
     class Settings:
         name = "users"
