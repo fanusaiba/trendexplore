cf2ba4d335cbe68af468cbf80cbea0b803575b19
Fanusaiba <nusaiba.fathima1703@gmail.com>
2026-03-15 20:10:03 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="TrendExplore API")
 origins = [
     "https://trendexplore.vercel.app",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    
 ]
 
 app.add_middleware(
